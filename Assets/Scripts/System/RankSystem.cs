012ec918776c7c35e619efccd14004c57416724c
Ish1koro <ytakeda2001@gmail.com>
2022-06-06 15:01:17 +0900

    ステージ制作

--- Assets/Scripts/System/RankSystem.cs
+++ Assets/Scripts/System/RankSystem.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Threading;
 using System.Threading.Tasks;
 
 public class RankSystem : MonoBehaviour
 {
-    private string url = "htttp://";
-    private void Start()
+    private UnityWebRequest request = UnityWebRequest.Get("http://");
+
+    async Task Start()
     {
+        yield return request.SendWebRequest();
+
+        await Start();
         
+        //3.isNetworkErrorとisHttpErrorでエラー判定
+        if (request.isHttpError || request.isNetworkError)
+        {
+            //4.エラー確認
+            Debug.Log(request.error);
+        }
     }
 }
